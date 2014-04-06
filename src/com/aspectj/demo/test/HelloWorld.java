45340cf88907ec82c7b6ac8d50fd6ae9fae1d6f8
Erfei Liu <andylau20017@gmail.com>
2014-04-06 23:57:12 +0800

    first commit

---
+++ src/com/aspectj/demo/test/HelloWorld.java
@@ -0,0 +1,13 @@
+package com.aspectj.demo.test;
+
+/**
+ * Created by erfeiliu on 2014/4/6.
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+      System.out.println("Hello World!");
+
+    }
+
+}
